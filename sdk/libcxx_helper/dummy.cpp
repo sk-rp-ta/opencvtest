64616783a7c1d9936194377865c40b98341ee6df
Milosz <milosz.sultanowski@gmail.com>
2022-03-21 19:41:58 +0100

    update

---
+++ sdk/libcxx_helper/dummy.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+
+// empty
+
+extern "C"
+JNIEXPORT jlong JNICALL
+Java_org_opencv_core_Mat_n_1setTo__JJJ(JNIEnv *env, jclass clazz, jlong native_obj,
+                                       jlong value_native_obj, jlong mask_native_obj) {
+    // TODO: implement n_setTo()
+}
